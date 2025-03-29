249e502808c23d191f65938dc07a552d167f0121
Jim-Yang98 <jublilant068@gmail.com>
2025-03-29 11:38:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,26 +19,30 @@ except ImportError:
 app = Flask(__name__)
 
 # 設定存放 PDF 的資料夾
-PDF_FOLDER = os.path.join(os.getcwd(), "static", "pdf")
+PDF_FOLDER = r"C:\Users\USER\Documents\my-sideproject\static\pdf"
+app.config["PDF_FOLDER"] = PDF_FOLDER  # 設定 Flask 配置
 os.makedirs(PDF_FOLDER, exist_ok=True)
 
 @app.route('/')
 def home():
     return '''
     <h1>PDF Viewer</h1>
-    <p><a href="/view-pdf/sample.pdf">點擊這裡查看 PDF</a></p>
-    <p><a href="/download-pdf/sample.pdf">點擊這裡下載 PDF</a></p>
+    <p><a href="/view-pdf/RFM.pdf">點擊這裡查看 PDF</a></p>
+    <p><a href="/show-pdf/RFM.pdf">內嵌 PDF 預覽</a></p>
     '''
 
+@app.route('/show-pdf/<filename>')
+def show_pdf(filename):
+    return f'''
+    <h1>PDF 內容</h1>
+    <iframe src="/view-pdf/{filename}" width="800" height="600"></iframe>
+    <p><a href="/download-pdf/{filename}">下載 PDF</a></p>
+    '''
+    
 # 提供 PDF 預覽
 @app.route('/view-pdf/<filename>')
 def view_pdf(filename):
     return send_from_directory(PDF_FOLDER, filename)
-
-# 提供 PDF 下載
-@app.route('/download-pdf/<filename>')
-def download_pdf(filename):
-    return send_from_directory(PDF_FOLDER, filename, as_attachment=True)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
